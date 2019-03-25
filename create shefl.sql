cec7e90fa40fc0a8b1dfa0c60712065944238c27
Huy Nguyen Ngoc <romeohuy@gmail.com>
2019-03-25 09:35:16 +0700

    * update script shefl helper

--- create shefl.sql
+++ create shefl.sql
@@ -1,16 +1,15 @@
 
 
-DELETE dbo.ShelfOrderItem
-DELETE dbo.Shelf
+--DELETE dbo.ShelfOrderItem
+--DELETE dbo.Shelf
 
 DECLARE @Letters NVARCHAR(MAX) = 'A,B,C,D,E,F,G,H,I,K,L,M,N,O,P,Q,R,S,T,V,X,Y,Z';
 CREATE TABLE #LetterTmp(
 	Letter NCHAR(1)
 );
-
 INSERT INTO #LetterTmp (Letter)
 	SELECT CAST(data as NCHAR(1)) FROM [nop_splitstring_to_table](@Letters, ',');
-
+go
 DECLARE @cnt INT = 1;
 
 WHILE @cnt < 6
@@ -28,34 +27,36 @@ BEGIN
 			FETCH NEXT 1 ROWS ONLY  
 			SET @i = @i + 1;
 			DECLARE @j INT = 101;
-			WHILE @j < 1000
+			WHILE @j < 131
 				BEGIN
-					IF @j != 200 AND  @j != 300 AND  @j != 400 AND  @j != 500 AND  @j != 600 AND  @j != 700 AND  @j != 800 AND  @j != 900    
-				
+					IF NOT EXISTS(SELECT * FROM dbo.Shelf WHERE ShelfCode = CONCAT(@cnt,@letterItem,@j))
+					BEGIN
 						PRINT(CONCAT(@cnt,@letterItem,@j));
-						INSERT INTO dbo.Shelf
-						(
-						    ShelfCode,
-						    CustomerId,
-						    AssignedDate,
-						    ShippedDate,
-						    IsCustomerNotified,
-						    ShelfNoteId,
-						    UpdatedNoteDate
-						)
-						VALUES
-						(   CONCAT(@cnt,@letterItem,@j),       -- ShelfCode - nvarchar(50)
-						    null,         -- CustomerId - int
-						    null, -- AssignedDate - datetime
-						    NULL, -- ShippedDate - datetime
-						    0,      -- IsCustomerNotified - bit
-						    0,         -- ShelfNoteId - int
-						    NULL  -- UpdatedNoteDate - datetime
-						    )
+							INSERT INTO dbo.Shelf
+							(
+								ShelfCode,
+								CustomerId,
+								AssignedDate,
+								ShippedDate,
+								IsCustomerNotified,
+								ShelfNoteId,
+								UpdatedNoteDate
+							)
+							VALUES
+							(   CONCAT(@cnt,@letterItem,@j),       -- ShelfCode - nvarchar(50)
+								null,         -- CustomerId - int
+								null, -- AssignedDate - datetime
+								NULL, -- ShippedDate - datetime
+								0,      -- IsCustomerNotified - bit
+								0,         -- ShelfNoteId - int
+								NULL  -- UpdatedNoteDate - datetime
+								)
+				   END
+						
 					SET @j = @j + 1;
 				END
 		END
    SET @cnt = @cnt + 1;
 END;
-
+go
 DROP TABLE #LetterTmp;
